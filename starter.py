818ab84891ed6bd2197c7cbf24ca62a0a87f2ef0
Wang Peiyu <oliviapy@udel.edu>
2017-11-30 22:05:07 -0500

    last assignment

--- starter.py
+++ starter.py
@@ -23,7 +23,7 @@ class Professor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     department = db.Column(db.Text)
-    courses = db.relationship('Course', backref='professor')##back reference at the many side, unique in ORM
+    courses = db.relationship('Course', backref='professor',cascade="delete")##back reference at the many side, unique in ORM
 
 @app.route('/index')
 def index():
@@ -65,6 +65,17 @@ def edit_professor(id):
         db.session.commit()
         return redirect(url_for('show_all_professors'))
 
+@app.route('/professor/delete/<int:id>', methods=['GET', 'POST'])
+def delete_professor(id):
+    professor = Professor.query.filter_by(id=id).first()
+    if request.method == 'GET':
+        return render_template('professor-delete.html', professor=professor)
+    if request.method == 'POST':
+        # delete the artist by id
+        # all related songs are deleted as well
+        db.session.delete(professor)
+        db.session.commit()
+        return redirect(url_for('show_all_professors'))
 
 @app.route('/courses')
 def show_all_courses():
@@ -104,6 +115,17 @@ def edit_course(number):
         professor = Professor.query.filter_by(name=professor_name).first()
         course.professor = professor
         # update the database
+        db.session.commit()
+        return redirect(url_for('show_all_courses'))
+
+@app.route('/course/delete/<int:number>', methods=['GET', 'POST'])
+def delete_course(number):
+    course = Course.query.filter_by(number=number).first()
+    professors = Professor.query.all()
+    if request.method == 'GET':
+        return render_template('course-delete.html', course=course, professors=professors)
+    if request.method == 'POST':
+        db.session.delete(course)
         db.session.commit()
         return redirect(url_for('show_all_courses'))
 
